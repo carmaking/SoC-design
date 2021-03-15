5a24e8cec508d52b4db5a63b81ec3cdc9eafdd57
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-15 23:12:06 +0900

    Create tb_vending_machine.v

---
+++ Verilog/tb_vending_machine.v
@@ -0,0 +1,45 @@
+`timescale  1ns /   1ps
+
+module tb_vending_machine();
+
+//vending machine 입력
+reg         rst_n;
+reg         clk;
+reg         coin, selection, refund;
+//vending machine 출력
+wire        beverage;
+
+vending_machine VM_u0(  .rst_n(rst_n),
+                        .clk(clk),  .coin(coin),
+                        .selection(selection),
+                        .refund(refund),
+                        
+                        .beverage(beverage));
+                        
+parameter   PERIOD_100MHZ   =   10;
+always begin
+    #(PERIOD_100MHZ/2);
+    clk <=  ~clk;
+end
+
+initial begin
+    rst_n       =   1'b1;
+    clk         =   1'b0;
+    coin        =   1'b0;
+    selection   =   1'b0;
+    refund      =   1'b0;
+    
+    #40 rst_n       =   1'b0;
+    #20 rst_n       =   1'b1;
+    #40 coin        =   1'b1;
+    #20 coin        =   1'b0;
+    #40 refund      =   1'b1;
+    #20 refund      =   1'b0;
+    #40 coin        =   1'b1;
+    #20 coin        =   1'b0;
+    #40 selection   =   1'b1;
+    #20 selection   =   1'b0;
+    #200    $stop;
+end
+
+endmodule
