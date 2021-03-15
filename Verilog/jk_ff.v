fc7a810f943d8d38cb12cf24b2e33eb229a6551c
carmaking <79752918+carmaking@users.noreply.github.com>
2021-03-15 23:10:45 +0900

    Create jk_ff.v

---
+++ Verilog/jk_ff.v
@@ -0,0 +1,29 @@
+module jk_ff(
+
+input       wire        clk,
+input       wire        j,
+input       wire        k,
+input       wire        rst_n,
+
+output      reg         q,
+output      wire        q_b
+);
+
+assign q_b = ~q;
+
+always @(posedge clk or negedge rst_n)
+begin
+    if(rst_n == 1'b0) begin
+        q <= 1'b0;
+    end
+    else begin
+        case({j,k})
+            2'b00 : q <= q;
+            2'b01 : q <= 1'b0;
+            2'b10 : q <= 1'b1;
+            2'b11 : q <= ~q;
+        endcase
+    end
+end
+
+endmodule
